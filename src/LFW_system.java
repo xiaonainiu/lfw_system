79c4b33c3712fbd4207579fb4cbfed850fef135f
xiaonainiu <es930119@gmail.com>
2017-09-14 23:19:58 +1000

    start of project

---
+++ src/LFW_system.java
@@ -0,0 +1,5 @@
+/**
+ * Created by ES on 2017/9/6.
+ */
+public class LFW_system {
+}
---
+++ src/order.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ES on 2017/9/6.
+ */
+public class order {
+    private String service_type;
+    private String address;
+    private int people;
+    private int[] luggage;
+}
